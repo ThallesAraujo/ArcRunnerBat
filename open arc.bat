e903edb5ca689f8c21de45c7761ac9c0a10e04cc
Thalles Araújo <thalleshnaraujo@brq.com>
2024-05-01 22:39:12 -0300

    Initial

---
+++ open arc.bat
@@ -0,0 +1,4 @@
+rd /s /q "C:\Users\<<your_user_folder_here>>\AppData\Local\Packages\TheBrowserCompany.Arc_ttt1ap7aakyb4\LocalCache\Local\firestore"
+rd /s /q "C:\Users\<<your_user_folder_here>>\AppData\Local\Packages\TheBrowserCompany.Arc_ttt1ap7aakyb4\LocalCache\Local\firebase-heartbeat"
+
+"C:\Program Files\WindowsApps\TheBrowserCompany.Arc_1.0.1.24682_x64__ttt1ap7aakyb4\Arc.exe"
---
+++ readme.md
@@ -0,0 +1,3 @@
+Simple .bat file for disabling firebase stuff on Arc which is preventing it from running on certain machines
+
+- Important note: replace the placeholder <<your_user_folder_here>> on the .bat before executing.
